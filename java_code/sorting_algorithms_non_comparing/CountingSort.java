cf91a7c123404450b792df329164816b7e56bd71
alex <alexburstein@gmail.com>
2020-07-22 14:00:51 +0300

    Minor updates for Counting sort and Radix sort

---
+++ java_code/sorting_algorithms_non_comparing/CountingSort.java
@@ -0,0 +1,14 @@
+package sorting_algorithms_non_comparing;
+
+import java.util.List;
+
+public class CountingSort<T> {
+    /**
+     * sorts the elements in the list. complexity: O(2n + k) time, and O(n + 2k) space. k is range of values
+     * @param list a list of Countable elements.
+     * @param <T> a type that implements Countable, or its subtype.
+     */
+    public static <T extends Countable> void countingSort(List<T> list){
+        new NonCompareSort<T>().CountingSortLogic(list);
+    }
+}
---
+++ java_code/sorting_algorithms_non_comparing/RadixSort.java
@@ -0,0 +1,15 @@
+package sorting_algorithms_non_comparing;
+
+import java.util.List;
+
+public class RadixSort<T>{
+    /**
+     * sorts the elements in the list. O(d * (2n + b)), and O(d * (n + 2b)) space.
+     *  d is number of buckets in k. b is size of each bucket.
+     * @param list a list of Countable elements.
+     * @param <T> a type that implements Countable, or its subtype.
+     */
+    public static <T extends Countable> void radixSort(List<T> list){
+        new NonCompareSort<T>().radixSortLogic(list);
+    }
+}
