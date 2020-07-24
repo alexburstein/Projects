0a6c205707820c998309b162bcc488026d00a069
WIN10 <alexburstein@gmail.com>
2020-07-24 22:00:43 +0300

    a cute quiz by code wars. Catching car millage interesting numbers.

--- java_code/logic_quizs/SudokuValidator.java
+++ java_code/logic_quizs/SudokuValidator.java
@@ -1,5 +1,13 @@
 package logic_quizs;
 
+/*
+    Sudoku is a game played on a 9x9 grid.
+    The goal of the game is to fill all cells of the grid with digits from 1 to 9,
+    so that each column, each row, and each of the nine 3x3 sub-grids (also known as blocks)
+    contain all of the digits from 1 to 9.
+
+ */
+
 import org.junit.jupiter.api.Test;
 import java.util.*;
 
